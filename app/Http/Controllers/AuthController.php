fbf7fb6f6fdc14ce6b8cf7f83601d81a5e63f46f
Stevanus Christian <stevanuschristian88@gmail.com>
2021-06-08 18:22:20 +0700

    Membuat Authentication dan Mengunakan API Token dengan Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Hash;
+use Session;
+use App\User;
+
+class AuthController extends Controller
+{
+    
+}
